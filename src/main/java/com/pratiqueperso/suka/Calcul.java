9255df01ea4289f9c0ca23c09d292a43840dcf22
Temoyala <>
2016-02-11 10:26:34 +0100

    Commit initial

---
+++ src/main/java/com/pratiqueperso/suka/Calcul.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pratiqueperso.suka;
+
+/**
+ *
+ * @author user
+ */
+public class Calcul {
+    protected int var1=0,varr=0;
+    
+    public double racineCaree(double valeur) {
+        
+        return Math.sqrt(valeur);
+        
+    }
+    
+    public int calculAB(int a,int b){
+        return a+b;
+    }
+    
+    
+}
